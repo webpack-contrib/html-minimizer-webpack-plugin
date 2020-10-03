8e236ef62c00cf9d73736516c557b06df230f232
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-10-03 18:21:07 +0300

    test: minify option

---
+++ test/minify-option.test.js
@@ -0,0 +1,40 @@
+import HtmlMinimizerPlugin from '../src/index';
+
+import {
+  compile,
+  getCompiler,
+  getErrors,
+  getWarnings,
+  readAssets,
+  removeCache,
+} from './helpers';
+
+describe('"minify" option', () => {
+  beforeEach(() => Promise.all([removeCache()]));
+
+  afterEach(() => Promise.all([removeCache()]));
+
+  it('should work minify function', async () => {
+    const testHtmlId = './simple.html';
+    const compiler = getCompiler(testHtmlId);
+
+    new HtmlMinimizerPlugin({
+      minimizerOptions: {
+        collapseWhitespace: true,
+      },
+      minify: (data, minimizerOptions) => {
+        // eslint-disable-next-line global-require
+        const htmlMinifier = require('html-minifier-terser');
+        const [[, input]] = Object.entries(data);
+
+        return htmlMinifier.minify(input, minimizerOptions);
+      },
+    }).apply(compiler);
+
+    const stats = await compile(compiler);
+
+    expect(readAssets(compiler, stats, /\.html$/i)).toMatchSnapshot('assets');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+  });
+});
